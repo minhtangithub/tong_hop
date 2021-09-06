94dce2d6e67c215d32931c4d9f85f77cfdbfc5a8
minh tan <nguyentan25021996hngu@gmail.com>
2021-09-06 20:53:08 +0700

    06/09/2021

---
+++ script.js
@@ -0,0 +1,76 @@
+function project (title, img, link) {
+    this.title = title;
+    this.img = img;
+    this.link = link;
+}
+
+const projects = [];
+
+
+projects.push(new project("expanding cards + progress step", './assets/img/1-expanding-cards.png', 'https://minhtangithub.github.io/50projects/1+2' ));
+projects.push(new project("rotating navigation", './assets/img/3-rotating-navigation-animation.png', 'https://minhtangithub.github.io/50projects/3' ));
+projects.push(new project("hidden search widget", './assets/img/4-hidden-search-widget.png', 'https://minhtangithub.github.io/50projects/4' ));
+projects.push(new project("blurry loading", './assets/img/5-blurry-loading.png', 'https://minhtangithub.github.io/50projects/5' ));
+projects.push(new project("scroll animation", './assets/img/6-scroll-animation.png', 'https://minhtangithub.github.io/50projects/6' ));
+projects.push(new project("split landing page", './assets/img/7-split-landing-page.png', 'https://minhtangithub.github.io/50projects/7' ));
+projects.push(new project("form wave", './assets/img/8-form-wave.png', 'https://minhtangithub.github.io/50projects/8' ));
+projects.push(new project("sound board", './assets/img/9-sound-board.png', 'https://minhtangithub.github.io/50projects/9' ));
+projects.push(new project("event keycodes", './assets/img/11-event-keycodes.png', 'https://minhtangithub.github.io/50projects/11' ));
+projects.push(new project("faq collapse", './assets/img/12-faq-collapse.png', 'https://minhtangithub.github.io/50projects/12' ));
+projects.push(new project("random choice picker", './assets/img/13-random-choice-picker.png', 'https://minhtangithub.github.io/50projects/13' ));
+projects.push(new project("animated navigation", './assets/img/14-animated-navigation.png', 'https://minhtangithub.github.io/50projects/14' ));
+projects.push(new project("incrementing counter", './assets/img/15-incrementing-counter.png', 'https://minhtangithub.github.io/50projects/15' ));
+projects.push(new project("drink water", './assets/img/16-drink-water.png', 'https://minhtangithub.github.io/50projects/16' ));
+projects.push(new project("movie app", './assets/img/17-movie-app.png', 'https://minhtangithub.github.io/50projects/17' ));
+projects.push(new project("background slider", './assets/img/18-background-slider.png', 'https://minhtangithub.github.io/50projects/18' ));
+projects.push(new project("theme clock", './assets/img/19-theme-clock.png', 'https://minhtangithub.github.io/50projects/19' ));
+projects.push(new project("button ripple effect", './assets/img/20-button-ripple-effect.png', 'https://minhtangithub.github.io/50projects/20' ));
+projects.push(new project("drag n drop", './assets/img/21-drag-n-drop.png', 'https://minhtangithub.github.io/50projects/21' ));
+projects.push(new project("kinetic-loader", './assets/img/23-kinetic-loader.png', 'https://minhtangithub.github.io/50projects/23' ));
+projects.push(new project("double vertical slider", './assets/img/26-double-vertical-slider.png', 'https://minhtangithub.github.io/50projects/26' ));
+projects.push(new project("double click heart", './assets/img/29-double-click-heart.png', 'https://minhtangithub.github.io/50projects/29' ));
+projects.push(new project("auto text effect", './assets/img/30-auto-text-effect.png', 'https://minhtangithub.github.io/50projects/30' ));
+projects.push(new project("password generator", './assets/img/31-password-generator.png', 'https://minhtangithub.github.io/50projects/31' ));
+projects.push(new project("good cheap fast", './assets/img/32-good-cheap-fast.png', 'https://minhtangithub.github.io/50projects/32' ));
+projects.push(new project("animated countdown", './assets/img/34-animated-countdown.png', 'https://minhtangithub.github.io/50projects/34' ));
+projects.push(new project("hover board", './assets/img/36-hoverboard.png', 'https://minhtangithub.github.io/50projects/36' ));
+projects.push(new project("mobile tab navigation", './assets/img/38-mobile-tab-navigation.png', 'https://minhtangithub.github.io/50projects/38' ));
+projects.push(new project("3d background boxes", './assets/img/40-3d-background-boxes.png', 'https://minhtangithub.github.io/50projects/40' ));
+projects.push(new project("verify account ui", './assets/img/41-verify-account-ui.png', 'https://minhtangithub.github.io/50projects/41' ));
+projects.push(new project("quiz app", './assets/img/46-quiz-app.png', 'https://minhtangithub.github.io/50projects/46' ));
+projects.push(new project("form validation", './assets/img/1.png', 'https://minhtangithub.github.io/20projects/1/' ));
+projects.push(new project("movie seat booking", './assets/img/2.png', 'https://minhtangithub.github.io/20projects/2/' ));
+projects.push(new project("custom video", './assets/img/3.png', 'https://minhtangithub.github.io/20projects/3/' ));
+projects.push(new project("exchange rate calculator", './assets/img/4.png', 'https://minhtangithub.github.io/20projects/4/' ));
+projects.push(new project("my landing page", './assets/img/6.png', 'https://minhtangithub.github.io/20projects/6/' ));
+projects.push(new project("hangman ", './assets/img/7.png', 'https://minhtangithub.github.io/20projects/7/' ));
+projects.push(new project("meal finder", './assets/img/8.png', 'https://minhtangithub.github.io/20projects/8/' ));
+projects.push(new project("music player", './assets/img/10.png', 'https://minhtangithub.github.io/20projects/10/' ));
+projects.push(new project("infinite blog", './assets/img/11.png', 'https://minhtangithub.github.io/20projects/11/' ));
+projects.push(new project("relaxer", './assets/img/16.png', 'https://minhtangithub.github.io/20projects/16/' ));
+projects.push(new project("shopee", './assets/img/shopee.png', 'https://minhtangithub.github.io/shopee' ));
+projects.push(new project("mp3 player", './assets/img/mp3.png', 'https://minhtangithub.github.io/F8-fullstack/MP3/index.html' ));
+projects.push(new project("hotel booking", './assets/img/hotel.png', 'https://minhtangithub.github.io/project1' ));
+projects.push(new project("the band", './assets/img/the-band.png', 'https://minhtangithub.github.io/F8-fullstack/w3%20band/index.html' ));
+
+
+
+
+
+
+console.log(projects)
+
+const row = document.querySelector('.row');
+projects.forEach(function(item) {
+    const {title , img, link} = item;
+    const div = document.createElement('div');
+    div.className = `col c-3-lg c-4-m c-6-sm`;
+    div.innerHTML = `
+                <img src="${img}">
+                <div class="content">
+                    <h4>${title}</h4>
+                    <a href="${link}">live demo</a>
+                </div>
+    `
+    row.appendChild(div);
+})
